c64aa7fe2c10336550260e5ad20dfc0091fd8bd2
Abrar Ajaz Wani <abhaywani114@gmail.com>
2021-04-08 23:55:17 +0530

    file to check login username and password

---
+++ src/checklogin.php
@@ -0,0 +1,19 @@
+<?php
+require_once __DIR__.'\lib\school.php';
+$admin = new Admin();
+if (isset($_POST['username']) and isset($_POST['password'])) {
+$username = mysqli_escape_string($admin->conn,$_POST['username']);
+$password = md5($_POST['password']);
+} else {
+    $password = NULL;
+    $username = NULL;
+}
+ if ($admin->authk($username,$password)) {
+      header ('Location: /index.php');
+ } else {
+     $error = 1;
+     require_once __DIR__.'\parts\head.php';
+     include ('login.php');
+     require_once __DIR__.'\parts\foot.php';
+}
+?>
